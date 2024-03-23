89c92d9998c43d097b2ccd59ddf88c6d2685c5c2
Shooker2012 <lovelywzz@gmail.com>
2024-03-23 21:43:54 +0800

    添加surround

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -95,6 +95,16 @@ local plugins = {
 		opts = {},
 		lazy = false,
 	},
+
+	-- Surround
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({ })
+		end
+	},
 }
 
 opts = nil
