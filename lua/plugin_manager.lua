7adf32c69edae10bef54ac8a04afc6b8956a8ea0
Shooker2012 <lovelywzz@gmail.com>
2024-03-23 22:17:05 +0800

    添加目录管理插件Neotree

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -105,6 +105,18 @@ local plugins = {
 			require("nvim-surround").setup({ })
 		end
 	},
+
+	-- Neotree
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	},
 }
 
 opts = nil
