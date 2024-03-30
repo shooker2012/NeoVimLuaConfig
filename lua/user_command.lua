e1141ed6fd7bb9b38d3506ceb69cd461a87756b9
Shooker2012 <lovelywzz@gmail.com>
2024-03-30 17:57:52 +0800

    迁移quick list和hex mode的一些功能

--- lua/user_command.lua
+++ lua/user_command.lua
@@ -32,3 +32,64 @@ local CopyMatches = function(args)
 end
 vim.api.nvim_create_user_command("CopyMatches", CopyMatches, { register=true, range="%"})
 vim.api.nvim_create_user_command("CM", CopyMatches, { register=true, range="%"})
+
+
+-- Map Quick Window
+local MapQuickFixWindow = function()
+	vim.cmd("botright copen")
+	vim.keymap.set("n", "t", "^<C-w>gF:Neotree reveal<CR>")
+end
+vim.api.nvim_create_user_command("Copen", MapQuickFixWindow, {})
+
+-- Hex mode
+vim.cmd([[
+" Hex mode start.==========
+" ex command for toggling hex mode - define mapping if desired
+command -bar Hexmode call ToggleHex()
+
+" helper function to toggle hex mode
+function ToggleHex()
+  " hex mode should be considered a read-only operation
+  " save values for modified and read-only for restoration later,
+  " and clear the read-only flag for now
+  let l:modified=&mod
+  let l:oldreadonly=&readonly
+  let &readonly=0
+  let l:oldmodifiable=&modifiable
+  let &modifiable=1
+  if !exists("b:editHex") || !b:editHex
+    " save old options
+    let b:oldft=&ft
+    let b:oldbin=&bin
+    " set new options
+    setlocal binary " make sure it overrides any textwidth, etc.
+    silent :e " this will reload the file without trickeries 
+              "(DOS line endings will be shown entirely )
+    let &ft="xxd"
+    " set status
+    let b:editHex=1
+    " switch to hex editor
+    %!xxd
+  else
+    " restore old options
+    let &ft=b:oldft
+    if !b:oldbin
+      setlocal nobinary
+    endif
+    " set status
+    let b:editHex=0
+    " return to normal editing
+    %!xxd -r
+  endif
+  " restore values for modified and read only state
+  let &mod=l:modified
+  let &readonly=l:oldreadonly
+  let &modifiable=l:oldmodifiable
+endfunction
+
+nnoremap <C-H> :Hexmode<CR>
+inoremap <C-H> <Esc>:Hexmode<CR>
+vnoremap <C-H> :<C-U>Hexmode<CR>
+" Hex mode end.==========
+]])
+
