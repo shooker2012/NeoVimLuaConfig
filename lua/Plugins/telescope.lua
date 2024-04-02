d71a7cc3b9ffc95b3af29ccb7fbf48f3bc568636
Shooker2012 <lovelywzz@gmail.com>
2024-04-02 23:59:28 +0800

    live grep时，禁卡c-u/c-d的翻页功能。（因为c-u清输入的功能更有用）

--- lua/Plugins/telescope.lua
+++ lua/Plugins/telescope.lua
@@ -33,8 +33,8 @@ telescope.setup{
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+        -- ["<C-u>"] = actions.preview_scrolling_up,
+        -- ["<C-d>"] = actions.preview_scrolling_down,
 
         ["<PageUp>"] = actions.results_scrolling_up,
         ["<PageDown>"] = actions.results_scrolling_down,
