f1e5b4cbc9b245a5195a8aa80de4d682cffc0395
lovelywzz@gmail.com <lovelywzz@gmail.com>
2025-04-18 16:38:32 +0800

    调整CopyFileName功能，添加新的CopyFilePath

--- lua/user_command.lua
+++ lua/user_command.lua
@@ -3,6 +3,17 @@ vim.api.nvim_create_user_command('CopyFileName', function(arg_table)
     if arg_table.args and arg_table.args ~= "" then
         p1 = arg_table.args
     else
+        p1 = ":t:r"
+    end
+    local fileName = vim.fn.expand("%"..p1)
+    vim.fn.setreg("*", fileName)
+end, { nargs='*' })
+
+vim.api.nvim_create_user_command('CopyFilePath', function(arg_table)
+    local p1
+    if arg_table.args and arg_table.args ~= "" then
+        p1 = arg_table.args
+    else
         p1 = ":p"
     end
     local fileName = vim.fn.expand("%"..p1)
