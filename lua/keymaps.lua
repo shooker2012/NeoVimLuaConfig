2c984e3c610737cbebbfa089cc6783dfeef4e441
lovelywzz@gmail.com <lovelywzz@gmail.com>
2026-09-24 12:21:28 +0800

    fix(nvim): stabilize visual F2 search selection
    - Read active visual selections with getregion to handle reversed and linewise ranges.
    - Keep visual F2 searches non-global while retaining all matching start lines.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,22 +19,25 @@ xnoremap # :<C-u>call <SID>VSetSearch()<CR>?<C-R>=@/<CR><CR>
 ]])
 
 local function search_visual_selection()
-	local start = vim.fn.getpos("'<")
-	local finish = vim.fn.getpos("'>")
-	if start[2] > finish[2] or (start[2] == finish[2] and start[3] > finish[3]) then
-		start, finish = finish, start
+	local visual_type = vim.fn.visualmode()
+	if visual_type == "" then
+		visual_type = vim.fn.mode()
 	end
-	local start_row, start_col = start[2] - 1, start[3] - 1
-	local end_row, end_col = finish[2] - 1, finish[3]
-	local lines = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
-	if #lines == 0 then
+	if visual_type ~= "v" and visual_type ~= "V" and visual_type ~= "\22" then
+		visual_type = "v"
+	end
+	local lines = vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), {
+		type = visual_type,
+		eol = true,
+	})
+	if not lines or #lines == 0 then
 		return
 	end
 	local literal = vim.fn.escape(table.concat(lines, "\n"), "\\")
 	literal = literal:gsub("\n", "\\n")
 	local regex = "\\V" .. literal
 	vim.fn.setreg("/", regex)
-	require("vimgrepbuffer").search(regex, { jump = false })
+	require("vimgrepbuffer").search(regex, { jump = false, global = false })
 end
 
 vim.keymap.set("n", "<F2>", function()
